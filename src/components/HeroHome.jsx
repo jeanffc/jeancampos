ebad435cd634bbaca4e33a688539d4c9ab921b3d
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2022-06-30 01:06:07 -0400

    feat: create home structure

---
+++ src/components/HeroHome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeroHome = () => {
+  return (
+    <div>HeroHome</div>
+  )
+}
+
+export default HeroHome
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
+import Header from '../components/Header';
+import HeroHome from '../components/HeroHome';
+import Footer from '../components/Footer';
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="flex flex-col min-h-screen overflow-hidden">
+
+      {/*  Site header */}
+      <Header />
+
+      {/*  Page content */}
+      <main className="flex-grow">
+
+        {/*  Page sections */}
+        <HeroHome />
+
+      </main>
+
+      {/*  Site footer */}
+      <Footer />
+
+    </div>
   )
 }
 
