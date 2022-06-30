56c20c2a9bfc8b16d73e7fce3f7f97cea2fb4916
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2022-06-30 01:37:37 -0400

    feat: custom hero home

--- src/components/HeroHome.jsx
+++ src/components/HeroHome.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
+// import Modal from '../utils/Modal';
+
+// import HeroImage from '../images/hero-image.png';
 
 const HeroHome = () => {
+
   return (
-    <div>HeroHome</div>
-  )
+    <section className="relative">
+
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+
+        {/* Hero content */}
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+
+          {/* Section header */}
+          <div className="text-center pb-12 md:pb-16">
+            <h1 className="text-6xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
+              My <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">website</span>
+            </h1>
+            <div className="max-w-3xl mx-auto">
+              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">
+                Description.
+              </p>
+              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
+                <div>
+                  <a className="btn px-5 py-3 text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="/signin">
+                    Sign in
+                  </a>
+                </div>
+                <div>
+                  <a className="btn px-5 py-3 text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="/signup">
+                    Sign up
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default HeroHome
+export default HeroHome;
