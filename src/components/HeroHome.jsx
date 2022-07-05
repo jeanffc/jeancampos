89b26bfc12058d37a46793e695237a3376e6fda5
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2022-07-04 21:15:54 -0400

    feat: updated hero home

--- src/components/HeroHome.jsx
+++ src/components/HeroHome.jsx
@@ -13,24 +13,15 @@ const HeroHome = () => {
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
             <h1 className="text-6xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
-              My <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">website</span>
+              Hi, this is <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Jean Campos</span>
             </h1>
             <div className="max-w-3xl mx-auto">
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">
-                Description.
+                I'm a Full Stack Developer with more than 10 years of experience in Leading and Developing projects.
+
+                I was responsible for delivering over 40 projects and campaigns in the areas of web, mobile, games, e-learning, it, and service desk, for clients such as Embraer, Volkswagen, Grupo Bimbo (Pullman, PlusVita, Ana Maria), Grupo Lonza (HTH), Aurora (Lindt, Jose Cuervo, The Macallan), Omni Financeira, Century 21, Txai Resorts, Prudence, mLabs (SaaS), among others.
+
               </p>
-              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
-                <div>
-                  <a className="btn px-5 py-3 text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="/signin">
-                    Sign in
-                  </a>
-                </div>
-                <div>
-                  <a className="btn px-5 py-3 text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="/signup">
-                    Sign up
-                  </a>
-                </div>
-              </div>
             </div>
           </div>
         </div>
