45fc27bc9380d2b00d33c3dfa98ef197afde3f44
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2022-07-06 22:13:24 -0400

    style: update service section spaces

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,7 +5,7 @@ import ServiceItem from './ServiceItem';
 const Services = () => {
   return (
     <div className="py-12 mx-12">
-      <h3 className='text-4xl font-extrabold leading-tighter tracking-tighter mb-4'>My Services</h3>
+      <h3 className='text-4xl font-extrabold leading-tighter tracking-tighter mb-8 pb-2 border-b'>My Services</h3>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
         {SERVICES.map(service => (
           <ServiceItem
