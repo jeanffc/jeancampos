c64adc9d47247164d452a8c271d9e659c503a8d4
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2022-07-06 22:17:28 -0400

    fix: change header background to white

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import { FaArrowRight } from 'react-icons/fa'
 
 const Header = () => {
   return (
-    <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out`}>
+    <header className={`fixed w-full z-50 bg-white border-b`}>
       <div className="max-w-6xl mx-auto px-5 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
 
