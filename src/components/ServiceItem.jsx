2f908f82e33c28a613ed112bd909df2e6a454bf4
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2022-07-06 22:14:20 -0400

    fix: change class to className

--- src/components/ServiceItem.jsx
+++ src/components/ServiceItem.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 const ServiceItem = ({ title, icon, description }) => {
   return (
-    <div class="p-1 shadow-xl bg-gradient-to-r from-blue-500 to-teal-400 rounded-2xl hover:-translate-y-2 transform transition">
-      <div class="block p-6 bg-white sm:p-8 rounded-xl">
-        <div class="sm:pr-8">
+    <div className="p-1 shadow-xl bg-gradient-to-r from-blue-500 to-teal-400 rounded-2xl hover:-translate-y-2 transform transition">
+      <div className="block p-6 bg-white sm:p-8 rounded-xl">
+        <div className="sm:pr-8">
           <div className="w-6 h-6 mb-3">
             {icon}
           </div>
-          <h5 class="text-xl font-bold text-gray-900">
+          <h5 className="text-xl font-bold text-gray-900">
             {title}
           </h5>
-          <p class="mt-2 text-sm text-gray-500">
+          <p className="mt-2 text-sm text-gray-500">
             {description}
           </p>
         </div>
