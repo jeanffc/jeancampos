1d179de588d701ed824d2b445f81679e47eaa79f
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2022-07-06 23:05:47 -0400

    feat: change services descriptions

--- src/data/services.jsx
+++ src/data/services.jsx
@@ -6,19 +6,19 @@ const SERVICES = [
     title: 'Ui Design',
     icon: <VscPreview className="w-full h-full" />,
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi rerum quia harum eaque, quae laboriosam quisquam distinctio.',
+      'In tandem with UX, I incorporate prototyping and animation to better help the users and engineers visualize how the product may act.',
   },
   {
     title: 'Web Design',
     icon: <VscEdit className="w-full h-full" />,
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi rerum quia harum eaque, quae laboriosam quisquam distinctio.',
+      'In tandem with UX, I incorporate prototyping and animation to better help the users and engineers visualize how the product may act.',
   },
   {
     title: 'Web Development',
     icon: <VscCode className="w-full h-full" />,
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi rerum quia harum eaque, quae laboriosam quisquam distinctio.',
+      'My web projects are responsive, accessible, and intuitive for all walks of life while reassuring they will respond the same on any browser.',
   },
 ];
 
