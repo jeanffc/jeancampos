b9660a42f2a1f4aa3cb38f171debe0a8e1c13a9b
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2022-07-06 22:13:56 -0400

    style: update work section spaces

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -5,7 +5,7 @@ import WorkItem from './WorkItem';
 function Works() {
   return (
     <div className="py-12 mx-12">
-      <h3 className='text-4xl font-extrabold leading-tighter tracking-tighter mb-4'>
+      <h3 className='text-4xl font-extrabold leading-tighter tracking-tighter mb-8 pb-2 border-b'>
         Favorite Works
       </h3>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
