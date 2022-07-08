b23d64e6d5a8104e1b81a66a18758d2b1b31c9a6
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2022-07-07 23:03:47 -0400

    fix: contact style

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,8 +8,8 @@ const Contact = () => {
       <h3 className='text-4xl font-extrabold leading-tighter tracking-tighter mb-8 pb-2 border-b'>
         Contact
       </h3>
-      <div className="flex flex-col items-start">
-        <p className='text-base text-gray-500 mb-4 '>
+      <div className="flex flex-col sm:flex-row items-start sm:items-center mx-auto">
+        <p className='flex-grow text-base text-gray-500 mb-4 '>
           Are you interested in collaborating or want to connect or chat? If so, hit the contact button down below or contact me via LinkedIn.
         </p>
         <a href="mailto:jeanffc@gmail.com" className='btn-md text-gray-200 bg-gray-900 hover:bg-gray-600 px-5 py-3 flex items-center transition duration-150 ease-in-out'>
