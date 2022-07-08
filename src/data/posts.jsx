29812d18e37a8d7f0a082d3effc395ec2b75ba8e
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2022-07-07 22:51:46 -0400

    feat: create blog page

--- src/data/posts.jsx
+++ src/data/posts.jsx
@@ -2,58 +2,58 @@ const POSTS = [
   {
     title: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit',
     category: 'CATEGORY',
-    date: '12 Jun 2019',
+    date: '12 Jun 2022',
     description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure cumque reprehenderit dolor facilis dolore soluta deserunt eum saepe, vero id odio! Soluta id aliquid illo nulla aliquam enim, quas corporis!',
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure cumque reprehenderit dolor facilis dolore soluta deserunt saepe, vero id odio! Soluta id aliquid nulla, quas corporis!',
   },
   {
     title: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit',
     category: 'CATEGORY',
-    date: '12 Jun 2019',
+    date: '08 Jun 2022',
     description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure cumque reprehenderit dolor facilis dolore soluta deserunt eum saepe, vero id odio! Soluta id aliquid illo nulla aliquam enim, quas corporis!',
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure reprehenderit dolor facilis dolore soluta deserunt eum saepe, vero id odio! Soluta id aliquid illo nulla aliquam enim, corporis!',
   },
   {
     title: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit',
     category: 'CATEGORY',
-    date: '12 Jun 2019',
+    date: '31 May 2022',
     description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure cumque reprehenderit dolor facilis dolore soluta deserunt eum saepe, vero id odio! Soluta id aliquid illo nulla aliquam enim, quas corporis!',
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure cumque dolor facilis dolore soluta deserunt eum saepe, vero id odio! Soluta id aliquid illo nulla aliquam enim, quas!',
   },
   {
     title: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit',
     category: 'CATEGORY',
-    date: '12 Jun 2019',
+    date: '12 May 2022',
     description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure cumque reprehenderit dolor facilis dolore soluta deserunt eum saepe, vero id odio! Soluta id aliquid illo nulla aliquam enim, quas corporis!',
+      'Lorem ipsum, dolor amet consectetur adipisicing elit. Iure cumque reprehenderit dolor facilis dolore soluta deserunt eum saepe, vero id odio! Soluta id aliquid illo nulla aliquam enim, quas corporis!',
   },
   {
     title: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit',
     category: 'CATEGORY',
-    date: '12 Jun 2019',
+    date: '02 May 2022',
     description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure cumque reprehenderit dolor facilis dolore soluta deserunt eum saepe, vero id odio! Soluta id aliquid illo nulla aliquam enim, quas corporis!',
+      'Lorem ipsum, dolor sit amet elit. Iure cumque reprehenderit dolor facilis dolore soluta deserunt eum saepe, vero id odio! Soluta id aliquid illo nulla aliquam enim, quas corporis!',
   },
   {
     title: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit',
     category: 'CATEGORY',
-    date: '12 Jun 2019',
+    date: '28 Apr 2022',
     description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure cumque reprehenderit dolor facilis dolore soluta deserunt eum saepe, vero id odio! Soluta id aliquid illo nulla aliquam enim, quas corporis!',
+      'Lorem ipsum, dolor sit amet consectetur adipisicing. Iure cumque reprehenderit facilis dolore soluta eum saepe, vero id odio! Soluta id aliquid illo nulla aliquam enim, quas!',
   },
   {
     title: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit',
     category: 'CATEGORY',
-    date: '12 Jun 2019',
+    date: '06 Apr 2022',
     description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure cumque reprehenderit dolor facilis dolore soluta deserunt eum saepe, vero id odio! Soluta id aliquid illo nulla aliquam enim, quas corporis!',
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure cumque dolor facilis dolore soluta deserunt eum saepe, vero id odio! Soluta id aliquid illo nulla aliquam enim, quas corporis!',
   },
   {
     title: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit',
     category: 'CATEGORY',
-    date: '12 Jun 2019',
+    date: '04 Apr 2022',
     description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure cumque reprehenderit dolor facilis dolore soluta deserunt eum saepe, vero id odio! Soluta id aliquid illo nulla aliquam enim, quas corporis!',
+      'Lorem ipsum, adipisicing. Iure cumque reprehenderit dolor facilis dolore soluta deserunt eum saepe! Soluta id aliquid illo nulla aliquam enim, quas corporis! Quas illo nulla aliquam',
   },
 
 ];
