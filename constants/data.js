fdb49e1ae2bba2cd5de74d1c8aec1e850af18b1d
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2022-08-07 16:18:12 -0400

    feat: add experience.

--- constants/data.js
+++ constants/data.js
@@ -44,46 +44,12 @@ const userData = {
   experience: [
     {
       title: "Software Developer",
-      company: "mroads",
+      company: "Route",
       year: "2021",
-      companyLink: "https://mroads.com",
-      desc: "Contributed to our flagship product - Panna - which is an AI powered candidate interviewing product.",
+      companyLink: "https://route.com",
+      desc: "...",
     },
-    {
-      title: "Founder and Developer",
-      company: "TailwindMasterKit",
-      year: "2021",
-      companyLink: "https://tailwindmasterkit.com",
-      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
-    },
-    {
-      title: "Founder and Developer",
-      company: "Algochurn",
-      year: "2021",
-      companyLink: "https://algochurn.com",
-      desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
-    },
-    {
-      title: "Internship",
-      company: "Intel Corporation",
-      year: "2019",
-      companyLink: "https://intel.com",
-      desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
-    },
-    {
-      title: "Graduation",
-      company: "UPES, Dehradun.",
-      year: "2019",
-      companyLink: "https://upes.co.in",
-      desc: "Major in Computer Science and Engineering with a CGPA of 7.0. Nobody asks this but it's okay.",
-    },
-    {
-      title: "High School",
-      company: "Delhi Public School, Jaipur.",
-      year: "2015",
-      companyLink: "https://upes.co.in",
-      desc: "Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.",
-    },
+
   ],
   resumeUrl:
     "https://www.canva.com/design/DADpzXJBp_8/HA_GLmWv9dj1qlQmMNnzoQ/view?utm_content=DADpzXJBp_8&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink",
